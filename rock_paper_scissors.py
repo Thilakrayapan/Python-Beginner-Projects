5dbbdf75f70454a0350e758dcc4f43fac0d13a6c
Thilakrayapan <rayapan41@gmail.com>
2026-06-19 15:45:08 +0530

    fix: Code Modularity Added, using funtions.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,18 +5,20 @@ choices = {
     'p':'Paper',
     's':'Scissors'
 }
+def get_user_choice():
+    while True:
+        user_choice = input("Enter rock paper scissors (r/p/s): ").lower()
+        if user_choice in choice:
+            return user_choice
+        else:
+            print("Invalid choice")
 
-while True:
-    user_choice = input("Enter rock paper scissors (r/p/s): ").lower()
-    computer_choice = random.choice(choice)
-    
-    print(f"User_choice: {choices[user_choice]}\nComputer_choice: {choices[computer_choice]}")
-    
-    if user_choice not in choice:
-        print('Invalid Choice!')
-        
+def display_choices(user_choice, computer_choice):
+        print(f"User_choice: {choices[user_choice]}\nComputer_choice: {choices[computer_choice]}")
+     
+def display_result(user_choice, computer_choice):
     if user_choice == computer_choice:
-        print("Result: Tie")   
+            print("Result: Tie")   
     elif (
       (user_choice == 'r' and computer_choice == 's') or
       (user_choice == 's' and computer_choice == 'p') or
@@ -25,13 +27,16 @@ while True:
         print("Result: You Win")   
     else:
         print("Result: You Lost")
+def play_game():   
+    while True:
+        user_choice = get_user_choice()
+        computer_choice = random.choice(choice)
+    
+        display_choices(user_choice, computer_choice)    
+        display_result(user_choice, computer_choice)  
+   
+        should_continue = input("If you continue (y/n): ").lower()
+        if should_continue == 'n':
+            break
         
-    should_continue = input("If you continue (y/n): ").lower()
-    if should_continue == 'n':
-        break
-    
-        
-    
-
-
-    
+play_game()
